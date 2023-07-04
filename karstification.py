71600ddab6991d80581d541d6ad2718a6988c4c8
Matt Covington <speleophysics@gmail.com>
2023-07-04 19:53:42 +0200

    reorganized file structure and qgis groups

--- karstification.py
+++ karstification.py
@@ -12,13 +12,13 @@ from gis_functions import clip_raster_to_geometry, clip_shp_to_geometry
 from sinkhole_functions import calc_karst_fraction
 
 
-def calc_karstification_for_HU10(HU10, sinkhole_dataset="USGS"):
+def calc_karstification_for_HU12(HU12, sinkhole_dataset="USGS", dem_res=5, project_dir='./qgis'):
     """
-    Download HU10 DEM and calculate karst index.
+    Download HU12 DEM and calculate karst index.
 
     Parameters
     ----------
-    HU10 : string - The number of the HU10 to run.
+    HU12 : pandas Series - A huc object from pynhd WaterData.
 
     sinkhole_dataset : string - The name of the sinkhole dataset to use.
                         Options are USGS or Mihevc.
@@ -29,16 +29,17 @@ def calc_karstification_for_HU10(HU10, sinkhole_dataset="USGS"):
     """
 
     # HU4 = HU10[:4]
-    rasterdir = "./NHD-data/"
+    huc12_str = HU12.huc12
+    rasterdir = os.path.join(project_dir, huc12_str)#"./NHD-data/"
+    os.makedirs(rasterdir)
     # huc10_str = HU10.uri.split("/")[-1]
     # huc10_str = HU10.iloc[0].huc10
-    huc10_str = HU10.huc12
-    rasterfile = huc10_str + ".tif"
+    rasterfile = huc12_str + ".tif"
 
     # hu10_geom = HU10.geometry #gpd.read_file('NHD-data/NHDPLUS_H_' + HU4 + '_HU4_GDB.gdb', layer='WBDHU10')
-    this_hu10 = HU10.geometry  # hu10[hu10.HUC10 == HU10]
+    this_hu12 = HU12.geometry  # hu10[hu10.HUC10 == HU10]
     dem = py3dep.get_map(
-        "DEM", this_hu10, resolution=30
+        "DEM", this_hu12, resolution=dem_res
     )  # , geo_crs="epsg:4326", crs="epsg:3857")
     dem.rio.to_raster(os.path.join(rasterdir, rasterfile))
 
@@ -62,13 +63,15 @@ def calc_karstification_for_HU10(HU10, sinkhole_dataset="USGS"):
         raise ValueError
 
     sinks = clip_shp_to_geometry(
-        clipname="HUC-" + huc10_str + "-sinks-",
+        clipname="HUC-" + huc12_str + "-sinks-",
         shpdir=sinks_dir,
+        outdir=rasterdir,
         shpfile=sinks_file,
-        geom_df=HU10,  # this_hu10,
+        geom_df=HU12,  # this_hu10,
         outcrs=imgsrc_elev.crs,
     )
-    huc_sinks = gpd.read_file("./HUC-" + huc10_str + "-sinks-" + polytag + ".shp")
+    sinks_shp = os.path.join(rasterdir, "./HUC-" + huc12_str + "-sinks-" + polytag + ".shp")
+    huc_sinks = gpd.read_file(sinks_shp)
     huc_sinks["ID"] = huc_sinks.index.values
     sinks_list = huc_sinks[["geometry", "ID"]].values.tolist()
     if len(sinks_list) == 0:
@@ -81,7 +84,7 @@ def calc_karstification_for_HU10(HU10, sinkhole_dataset="USGS"):
             sinks_list, fill=0, out_shape=out_shape, transform=out_trans
         )
         profile = imgsrc_elev.profile
-        sinks_raster = "HUC-" + huc10_str + "-sinks-SinkholePolys.tif"
+        sinks_raster = os.path.join(rasterdir,"HUC-" + huc12_str + "-sinks-SinkholePolys.tif")
         with rasterio.open(sinks_raster, "w", **profile) as dest:
             dest.write(sinks_array.astype(rasterio.int32), 1)
 
