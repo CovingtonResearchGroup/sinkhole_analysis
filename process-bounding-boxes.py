ac7a664715a11373e707e80cdaa660e5144c3899
Matt Covington <speleophysics@gmail.com>
2023-08-08 13:33:32 -0500

    updated bbox processing script py file

--- process-bounding-boxes.py
+++ process-bounding-boxes.py
@@ -1,14 +1,25 @@
+import os
+
+os.environ["USE_PYGEOS"] = "0"
 import geopandas as gpd
 from karstification import calc_karstification_for_HU12
 from pandas import read_excel
 from pynhd import pynhd
+import py3dep
 from shapely import box
 import os
 import string
 import warnings
+import multiprocessing
+import pickle
+from random import randint
+import time
 
 hr = pynhd.NHDPlusHR("huc12")
 huc12 = pynhd.WaterData("wbd12", crs="epsg:4326")
+n_processes = 4
+box_df = read_excel("bounding_boxes.xlsx")
+bbox_zip = zip(box_df.x_min, box_df.y_min, box_df.x_max, box_df.y_max)
 
 
 def format_filename(s):
@@ -27,38 +38,66 @@ def format_filename(s):
     return filename
 
 
-box_df = read_excel("bounding_boxes.xlsx")
-bbox_zip = zip(box_df.x_min, box_df.y_min, box_df.x_max, box_df.y_max)
-
-hucs_list = []
-p_karst_list = []
-
-for i, bbox in enumerate(bbox_zip):
-    if i > 1:
-        print(asdf)
+def process_box(bbox_enum):
+    # Randomize query times to that they don't come at once
+    sleeptime = randint(1, 10)
+    time.sleep(sleeptime)
+    p_karst_list = []
+    i, bbox = bbox_enum
     boxname = box_df.Name[i]
     print("Processing", boxname)
     box_hucs12 = huc12.bybox(bbox)
-    box_poly = box(*bbox)
-    box_gdf = gpd.GeoDataFrame(index=[0], crs="epsg:4326", geometry=[box_poly])
-    box_dirname = format_filename(boxname)
-    box_path = os.path.join("./qgis/", box_dirname)
-    if not os.path.exists(box_path):
-        os.makedirs(box_path)
-    box_gdf.to_file(os.path.join(box_path, box_dirname + ".shp"))
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        box_hucs12.to_file(os.path.join(box_path, "box_hucs.shp"))
+    # Check available resolution
+    avail = py3dep.check_3dep_availability(bbox)
+    if avail["3m"] == True:
+        dem_res = 3
+        found_res = True
+    elif avail["5m"] == True:
+        dem_res = 5
+        found_res = True
+    elif avail["10m"] == True:
+        dem_res = 10
+        found_res = True
+    else:
+        found_res = False
+    if found_res:
+        print("Using dem resolution", str(dem_res))
+        box_poly = box(*bbox)
+        box_gdf = gpd.GeoDataFrame(index=[0], crs="epsg:4326", geometry=[box_poly])
+        box_dirname = format_filename(boxname)
+        box_path = os.path.join("./qgis/", box_dirname)
+        if not os.path.exists(box_path):
+            os.makedirs(box_path)
+        box_gdf.to_file(os.path.join(box_path, box_dirname + ".shp"))
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            box_hucs12.to_file(os.path.join(box_path, "box_hucs.shp"))
 
-    for hu_idx in box_hucs12.index:
-        hu = box_hucs12.iloc[hu_idx]
-        huc_num = hu.huc12
-        print("HUC", huc_num)
-        hu.crs = "EPSG:4326"
-        p_karst = calc_karstification_for_HU12(hu, boxname=box_dirname)
-        hucs_list.append(huc_num)
-        p_karst_list.append(p_karst)
+        for hu_idx in box_hucs12.index:
+            hu = box_hucs12.iloc[hu_idx]
+            huc_num = hu.huc12
+            print("HUC", huc_num)
+            hu.crs = "EPSG:4326"
+            p_karst = calc_karstification_for_HU12(hu, boxname=box_dirname)
+            p_karst_list.append(p_karst)
 
-        print(
-            "HU", huc_num, "has", str(p_karst)[:5], "percent internal karst drainage."
-        )
+            print(
+                "HU",
+                huc_num,
+                "has",
+                str(p_karst)[:5],
+                "percent internal karst drainage.",
+            )
+
+    else:
+        print("No dem available at required resolutions.")
+        p_karst_list.append(-1)
+
+    box_hucs12["p_karst"] = p_karst_list
+    output_hucs_file = os.path.join(box_path, "bbox_df.csv")
+    box_hucs12.to_csv(output_hucs_file)
+    return box_hucs12
+
+
+with multiprocessing.Pool(processes=8) as pool:
+    pool.map(process_box, enumerate(bbox_zip))
