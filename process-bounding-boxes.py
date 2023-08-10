caf5240134bfb359371af8a740e904bb2e3157e0
Matt Covington <speleophysics@gmail.com>
2023-08-10 14:27:39 -0500

    skip boxes with existing csvs

--- process-bounding-boxes.py
+++ process-bounding-boxes.py
@@ -20,6 +20,7 @@ hr = pynhd.NHDPlusHR("huc12")
 huc12 = pynhd.WaterData("wbd12", crs="epsg:4326")
 box_df = read_excel("bounding_boxes.xlsx")
 bbox_zip = zip(box_df.x_min, box_df.y_min, box_df.x_max, box_df.y_max)
+project_dir = "./qgis"
 
 
 def format_filename(s):
@@ -38,7 +39,7 @@ def format_filename(s):
     return filename
 
 
-def process_box(bbox_enum):
+def process_box(bbox_enum, overwrite=False):
     # Randomize query times to that they don't come at once
     sleeptime = randint(1, 10)
     time.sleep(sleeptime)
@@ -46,60 +47,69 @@ def process_box(bbox_enum):
     i, bbox = bbox_enum
     boxname = box_df.Name[i]
     print("Processing", boxname)
-    box_hucs12 = huc12.bybox(bbox)
-    # Check available resolution
-    avail = py3dep.check_3dep_availability(bbox)
-    if avail["3m"] == True:
-        dem_res = 3
-        found_res = True
-    elif avail["5m"] == True:
-        dem_res = 5
-        found_res = True
-    elif avail["10m"] == True:
-        dem_res = 10
-        found_res = True
+    # Check if box csv already exists
+    box_dirname = format_filename(boxname)
+    box_path = os.path.join("./qgis/", box_dirname)
+    csv_exists = False
+    if not overwrite:
+        if os.path.isfile(os.path.join(box_path, "bbox_df.csv")):
+            csv_exists = True
+
+    if overwrite or not csv_exists:
+        box_hucs12 = huc12.bybox(bbox)
+        # Check available resolution
+        avail = py3dep.check_3dep_availability(bbox)
+        if avail["3m"] == True:
+            dem_res = 3
+            found_res = True
+        elif avail["5m"] == True:
+            dem_res = 5
+            found_res = True
+        elif avail["10m"] == True:
+            dem_res = 10
+            found_res = True
+        else:
+            found_res = False
+        if found_res:
+            print("Using dem resolution", str(dem_res))
+            box_poly = box(*bbox)
+            box_gdf = gpd.GeoDataFrame(index=[0], crs="epsg:4326", geometry=[box_poly])
+            if not os.path.exists(box_path):
+                os.makedirs(box_path)
+
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                box_gdf.to_file(os.path.join(box_path, box_dirname + ".shp"))
+                box_hucs12.to_file(os.path.join(box_path, "box_hucs.shp"))
+
+            for hu_idx in box_hucs12.index:
+                hu = box_hucs12.iloc[hu_idx]
+                huc_num = hu.huc12
+                print("HUC", huc_num)
+                hu.crs = "EPSG:4326"
+                p_karst = calc_karstification_for_HU12(
+                    hu, boxname=box_dirname, dem_res=dem_res
+                )
+                p_karst_list.append(p_karst)
+
+                print(
+                    "HU",
+                    huc_num,
+                    "has",
+                    str(p_karst)[:5],
+                    "percent internal karst drainage.",
+                )
+
+        else:
+            print("No dem available at required resolutions.")
+            p_karst_list.append(-1)
+
+        box_hucs12["p_karst"] = p_karst_list
+        output_hucs_file = os.path.join(box_path, "bbox_df.csv")
+        box_hucs12.to_csv(output_hucs_file)
+        return box_hucs12
     else:
-        found_res = False
-    if found_res:
-        print("Using dem resolution", str(dem_res))
-        box_poly = box(*bbox)
-        box_gdf = gpd.GeoDataFrame(index=[0], crs="epsg:4326", geometry=[box_poly])
-        box_dirname = format_filename(boxname)
-        box_path = os.path.join("./qgis/", box_dirname)
-        if not os.path.exists(box_path):
-            os.makedirs(box_path)
-
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            box_gdf.to_file(os.path.join(box_path, box_dirname + ".shp"))
-            box_hucs12.to_file(os.path.join(box_path, "box_hucs.shp"))
-
-        for hu_idx in box_hucs12.index:
-            hu = box_hucs12.iloc[hu_idx]
-            huc_num = hu.huc12
-            print("HUC", huc_num)
-            hu.crs = "EPSG:4326"
-            p_karst = calc_karstification_for_HU12(
-                hu, boxname=box_dirname, dem_res=dem_res
-            )
-            p_karst_list.append(p_karst)
-
-            print(
-                "HU",
-                huc_num,
-                "has",
-                str(p_karst)[:5],
-                "percent internal karst drainage.",
-            )
-
-    else:
-        print("No dem available at required resolutions.")
-        p_karst_list.append(-1)
-
-    box_hucs12["p_karst"] = p_karst_list
-    output_hucs_file = os.path.join(box_path, "bbox_df.csv")
-    box_hucs12.to_csv(output_hucs_file)
-    return box_hucs12
+        print(boxname, "csv file already exists. Skipping.")
 
 
 if __name__ == "__main__":
